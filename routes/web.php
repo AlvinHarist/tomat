fc03ea5d8f6a556479d1a6518005c19f0e2fd062
AlvinHarist <alvinharist57@gmail.com>
2025-11-23 14:08:35 +0700

    konfigurasi SMTP Email

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ProductController;
+use App\Mail\SendTestEmail;
 
 /*
 |--------------------------------------------------------------------------
@@ -49,6 +51,12 @@ Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+
+Route::get('send-mail', function() {
+    $message = 'Salam Tomat';
+    Mail::to('alvin.harist502@gmail.com')->send(new SendTestEmail($message));
+});
 
 Route::get('/home', [ProductController::class, 'index'])->name('home');
 Route::resource('product', ProductController::class)->except(['index']);
